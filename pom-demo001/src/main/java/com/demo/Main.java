fe22c6c4ff4ff13b5f9aef2b420baf5d53c9e870
jiancheng <wangjc25@lenovo.com>
2021-07-28 00:13:57 +0800

    学习pom相关知识

---
+++ pom-demo001/src/main/java/com/demo/Main.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+/**
+ * 类描述
+ *
+ * @author jcwang
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
